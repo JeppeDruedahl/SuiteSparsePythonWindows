566c92313240107dc4f4bd17310a5a7311cf70cd
Jeppe Druedahl <jeppe.druedahl@econ.ku.dk>
2019-08-12 09:50:00 +0200

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -1,4 +1,3 @@
-cd "C:\SuiteSparsePythonWindows\build"
 cd C:/Program Files (x86)/Microsoft Visual Studio/2017/Community/VC/Auxiliary/Build/
 call vcvarsall.bat x64
 cd "C:\SuiteSparsePythonWindows\build\"
